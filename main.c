0f4bdba64f4982e5a4d54b24881a1017fbd195c6
UgwuRhema <rhemauros@protonmail.com>
2026-09-11 13:29:18 +0000

    Just felt like splitting it into a header and a main, i haven't done that in a long time

--- main.c
+++ main.c
@@ -3,35 +3,10 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdbool.h>
+#include "pass.h"
 
 #define MAX_PASS 256
 #define PERMS 0660
-
-struct Passwd
-{
-    char *key; //what you'll use to access this password...so no intruder can see it...
-    char *password;
-    char *host;
-    int id;
-};
-
-struct Passwd* new_password(char *key, char *password, char *host, int id)
-{
-    struct Passwd *psk = (struct Passwd*)malloc(sizeof(struct Passwd));
-    psk->key = strdup(key);
-    psk->password = strdup(password);
-    psk->host = strdup(host);
-    psk->id = id;
-    return psk;
-}
-
-//add a password to the global list
-void addPasswordToList(struct Passwd **list, struct Passwd *password, int *offset)
-{
-    if (list == NULL || password == NULL || *offset >= MAX_PASS) return;
-    list[*offset] = password;
-    (*offset)++;
-}
 
 [[maybe_unused]]static struct Passwd *passwords[MAX_PASS];
 
---
+++ pass.h
@@ -0,0 +1,26 @@
+struct Passwd
+{
+    char *key; //what you'll use to access this password...so no intruder can see it...
+    char *password;
+    char *host;
+    int id;
+};
+
+struct Passwd* new_password(char *key, char *password, char *host, int id)
+{
+    struct Passwd *psk = (struct Passwd*)malloc(sizeof(struct Passwd));
+    psk->key = strdup(key);
+    psk->password = strdup(password);
+    psk->host = strdup(host);
+    psk->id = id;
+    return psk;
+}
+
+//add a password to the global list
+void addPasswordToList(struct Passwd **list, struct Passwd *password, int *offset)
+{
+    if (list == NULL || password == NULL || *offset >= MAX_PASS) return;
+    list[*offset] = password;
+    (*offset)++;
+}
+
